e9435f0614919d7e54cacb87db53cdd63bb1b595
diego-mobilemed-dev <diego.souza@mobilemed.com.br>
2025-11-22 21:45:11 -0300

    mudanca na chamada o module articles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { appModules } from 'src/interface/modules/index.modules';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { sequelizeConfig } from 'src/config/sequelize.config';
 import { ConfigModule } from '@nestjs/config';
+import { ArticleModule } from './interface/modules/articles.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     SequelizeModule.forRoot(sequelizeConfig),
     ...appModules,
+    ArticleModule
   ],
 })
 export class AppModule  {}
