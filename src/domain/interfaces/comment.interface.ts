dca50942cee681747fff6440e9d130b046a36370
diego-mobilemed-dev <diego.souza@mobilemed.com.br>
2026-01-13 22:28:31 -0300

    fix: correção de erro deploy

--- src/domain/interfaces/comment.interface.ts
+++ src/domain/interfaces/comment.interface.ts
@@ -1,7 +1,7 @@
 import { Comment } from '../../infrastructure/database/sequelize/models/comment.model';
 import { User } from '../../infrastructure/database/sequelize/models/user.model';
 
-export interface CommentWithUser extends Comment {
+export interface CommentWithUser extends Omit<Comment, 'user' | 'replies'> {
   user?: User;
   userLiked?: boolean;
   userDisliked?: boolean;
